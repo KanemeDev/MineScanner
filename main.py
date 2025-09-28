7974826592f418bfbb9535b66efe95d8ce79d948
Showdown76py <showdown76.a@gmail.com>
2025-09-29 00:49:13 +0200

    fix: text formatting

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ def write_result(text: str):
             with open('result.txt', 'a', encoding='utf-8') as f:
                 f.write(text + "\n")
     except Exception as e:
-        print(f"Failed to write : {e}")
+        print(f"Failed to write: {e}")
 
 #ip range expansion
 def expand_iprange(iprange: str):
@@ -120,7 +120,7 @@ def scan_ip(ip: str, port_range):
         msg = f"MineScanner: {ip} has responded to ping"
         print(msg + "\n")
 
-        print(f"Scanning {ip} (started at {datetime.now().strftime('%H:%M:%S')})")
+        print(f"MineScanner: Scanning {ip} (started at {datetime.now().strftime('%H:%M:%S')})")
 
         try:
             def port_worker(port: int):
@@ -178,7 +178,7 @@ def scan_ip(ip: str, port_range):
         except socket.error:
             print("Couldn't connect to server")
     else:
-        print(f"MineScanner : {ip} unreachable\n")
+        print(f"MineScanner: {ip} is unreachable")
 
 
 if __name__ == '__main__':
